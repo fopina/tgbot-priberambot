d0ca005125d94f408b5c01a5abc2d211db2f0853
Filipe Pina <fopina@gmail.com>
2016-01-11 16:47:17 +0000

    inline test

--- tests/test_priberam.py
+++ tests/test_priberam.py
@@ -2,39 +2,28 @@
 from tgbot import plugintest
 from tgbot.botapi import Update
 from plugins.priberam import PriberamPlugin
+
 from requests.packages import urllib3
 urllib3.disable_warnings()
 
 
+class FakeInlineTelegramBot(plugintest.FakeTelegramBot):
+    # TODO - improve this and add it to tgbotplug
+    def answer_inline_query(self, inline_query_id, results, cache_time=None, is_personal=None, next_offset=None, **kwargs):
+        self._sent_messages.append(([inline_query_id, results], kwargs))
+        self._current_message_id += 1
+        return FakeInlineTelegramBot.FakeRPCRequest(True)
+
+
 class PriberamPluginTest(plugintest.PluginTestCase):
     def setUp(self):
-        self.bot = self.fake_bot('', plugins=[PriberamPlugin()])
-        self.received_id = 1
-
-    def receive_message(self, text, sender=None, chat=None):
-        if sender is None:
-            sender = {
-                'id': 1,
-                'first_name': 'John',
-                'last_name': 'Doe',
-            }
-
-        if chat is None:
-            chat = sender
-
-        self.bot.process_update(
-            Update.from_dict({
-                'update_id': self.received_id,
-                'message': {
-                    'message_id': self.received_id,
-                    'text': text,
-                    'chat': chat,
-                    'from': sender,
-                }
-            })
+        self.bot = FakeInlineTelegramBot(
+            '',
+            plugins=[PriberamPlugin()],
+            inline_query=PriberamPlugin(),
+            no_command=PriberamPlugin(),
         )
-
-        self.received_id += 1
+        self.received_id = 1
 
     def test_need_reply(self):
         self.receive_message('/priberam')
@@ -74,3 +63,59 @@ s. m.
  Indivíduo cuja estatura é inferior à dos homens em geral.
 Feminino: anã. Plural: anãos ou anões.\
 ''')
+
+    def test_inline(self):
+        self.receive_inline(u'bola')
+        results = self.last_reply(self.bot)
+        self.assertEqual(
+            [x.title for x in results],
+            [u'bola', u'bola de cristal', u'bola de gude', u'bola de neve', u'bola-ao-cesto', u'bolacha', u'bolachada', u'bolacheira']
+        )
+
+    def receive_message(self, text, sender=None, chat=None):
+        if sender is None:
+            sender = {
+                'id': 1,
+                'first_name': 'John',
+                'last_name': 'Doe',
+            }
+
+        if chat is None:
+            chat = sender
+
+        self.bot.process_update(
+            Update.from_dict({
+                'update_id': self.received_id,
+                'message': {
+                    'message_id': self.received_id,
+                    'text': text,
+                    'chat': chat,
+                    'from': sender,
+                }
+            })
+        )
+
+        self.received_id += 1
+
+    def receive_inline(self, query, sender=None):
+        if sender is None:
+            sender = {
+                'id': 1,
+                'first_name': 'John',
+                'last_name': 'Doe',
+            }
+
+        self.bot.process_update(
+            Update.from_dict({
+                'update_id': self.received_id,
+                'message': None,
+                'inline_query': {
+                    'id': self.received_id,
+                    'query': query,
+                    'from': sender,
+                    'offset': '',
+                }
+            })
+        )
+
+        self.received_id += 1
